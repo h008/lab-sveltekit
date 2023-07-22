ffb738e348d53c024e7f1016181f0e239217aae1
h_008 <h_008@mac.com>
2023-07-22 09:24:05 +0900

    event.locals,$page,dataの関係性実験

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+import type { RequestEvent } from "@sveltejs/kit";
+
+export async function load(event: RequestEvent) {
+    // load関数でセットしたデータもフロント$page.dataで取得できる
+    const posts = [{ title: "test", description: "test" }]
+    return { posts }
+
+
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<p>$page</p>
+	{JSON.stringify($page)}
+</div>
+<div>
+	<p>data</p>
+	{JSON.stringify(data)}
+</div>
